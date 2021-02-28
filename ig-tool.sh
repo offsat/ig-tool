27c1a3a1d34678cf127eacf097fc4303bb4b12f8
Meow404 <73886809+penucuriCode@users.noreply.github.com>
2021-02-28 15:12:22 +0000

    Update ig-tool.sh

--- ig-tool.sh
+++ ig-tool.sh
@@ -36,16 +36,16 @@ read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' optio
 
 
 if [[ $option == 1 || $option == 01 ]]; then
-cd $HOME/ighack/main
+cd $HOME/ig-tool/main
 bash auto.sh
 elif [[ $option == 2 || $option == 02 ]]; then
-cd $HOME/ighack/main
+cd $HOME/ig-tool/main
 bash manual.sh
 elif [[ $option == 3 || $option == 03 ]]; then
-cd $HOME/ighack/core
+cd $HOME/ig-tool/core
 bash about.sh
 elif [[ $option == 4 || $option == 04 ]]; then                                                                                                                
-cd $HOME/ighack/core
+cd $HOME/ig-tool/core
 bash update.sh
 elif [[ $option == 5 ]]; then
 exit 1
